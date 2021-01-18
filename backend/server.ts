56c8e3cb3db53877dc8f683b3f75a5cac904a6dd
drahunpavel <drahunpavel@gmail.com>
2021-01-18 17:01:44 +0300

    Добавлена модель пользователя и другие изменений

--- backend/server.ts
+++ backend/server.ts
@@ -3,11 +3,15 @@ import express from 'express';
 const app = express();
 
 // app.get('/hello', (req: express.Request, res: express.Response) => {
-app.get('/hello', (_, res: express.Response) => {
-    res.send('Hello');
-});
+// app.get('/hello', (_, res: express.Response) => {
+//     res.send('Hello');
+// });
 
-app.listen(8080, () => {
+// app.get('/users', (req: express.Request, res: express.Response) => {
+//     res.send('Hello');
+// });
+
+app.listen(8081, (): void => {
     // if(err){
     //     throw new Error(err);
     // }
