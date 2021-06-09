18fc5016265f991b969d49219149ae2922691547
drahunpavel <drahunpavel@gmail.com>
2021-06-09 18:51:49 +0300

    Добавлены TweetModelInterface, TweetModelDocumentInterface, TweetSchema, TweetModel

---
+++ backend/models/TweetModel.ts
@@ -0,0 +1,28 @@
+import { model, Schema, Document } from "mongoose";
+
+export interface TweetModelInterface extends Document {
+  _id?: string;
+  text: string;
+  user: string;
+};
+
+// отдельный интерфейс, который содержит и TweetModelInterface, и Document-интерфейс из mongoose
+// для того, чтобы у сущности были свойства и от TweetModelInterface, и от mongoose
+// Document - типизация из mongoose
+export type TweetModelDocumentInterface = TweetModelInterface & Document;
+
+//схема для монги
+const TweetSchema = new Schema<TweetModelInterface>({
+  text: {
+    required: true, //обязательность поля
+    type: String,
+  },
+  user: {
+    required: true,
+    ref: 'User', // этот ObjectId ссылается на модель User
+    type: Schema.Types.ObjectId, // ObjectId нашего пользователя
+    //Schema.Types.ObjectId с помощью этой записи можно делать связь в БД между нашими моделями 
+  }
+});
+
+export const TweetModel = model<TweetModelDocumentInterface>("Tweet", TweetSchema);
