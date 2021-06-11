43f86ef0fcee3164cedd45ddd071ca80aa680b20
drahunpavel <drahunpavel@gmail.com>
2021-06-12 00:07:48 +0300

    добавлена нотификация при логине польователя

--- frontend/src/pages/SignIn/components/LoginModal.tsx
+++ frontend/src/pages/SignIn/components/LoginModal.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Button, FormControl, FormGroup, TextField } from '@material-ui/core';
 import { ModalBlock } from '../../../Components/ModalBlock';
+import { Color } from '@material-ui/lab/Alert';
 
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'; //прослойка между react-hook-form и yup
 import * as yup from "yup";
 
 import { useStylesSignIn } from '../SignIn';
+import { AuthApi } from '../../../services/api/authApi';
+import { Notification } from '../../../Components/Notification';
 
 interface LoginModalProps {
     open: boolean;
@@ -14,7 +17,7 @@ interface LoginModalProps {
     // classes: ReturnType<typeof useStylesSignIn>;
 };
 
-interface LoginModalFormProps {
+export interface LoginModalFormProps {
     email: string;
     password: string;
 };
@@ -27,82 +30,103 @@ const LoginFormSchema = yup.object().shape({
 const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }): React.ReactElement => {
     const classes = useStylesSignIn();
 
+    const openNotificationRef = React.useRef<(text: string, type: Color) => void>(() => { });
+
     const { control, handleSubmit, formState: { errors } } = useForm<LoginModalFormProps>({
         resolver: yupResolver(LoginFormSchema)
     });
 
-    const onSubmit = (data: LoginModalFormProps) => console.log(data);
-
     console.log('--errors', errors)
 
-    return (
-        <ModalBlock
-            visible={open}
-            onClose={(onClose)}
-            classes={classes}
-            title="Войти в аккаунт">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
-                    <FormGroup aria-label="position" row>
-                        <Controller
-                            control={control}
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <TextField
-                                    className={classes.loginSideField}
-                                    id="email"
-                                    label="E-Mail"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="filled"
-                                    type="email"
-                                    // defaultValue=""
-                                    helperText={errors.email?.message}
-                                    error={!!errors.email}
-                                    fullWidth
-                                    autoFocus
-                                />
-                            )}
-                            name="email"
-                            rules={{ required: true }}
-                            defaultValue=""
-                        />
-                        <Controller
-                            control={control}
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <TextField
-                                    className={classes.loginSideField}
-                                    id="password"
-                                    label="Пароль"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="filled"
-                                    type="password"
-                                    defaultValue=""
-                                    helperText={errors.password?.message}
-                                    error={!!errors.password}
-                                    fullWidth
-                                />
-                            )}
-                            name="password"
-                            rules={{ required: true }}
-                            defaultValue=""
-                        />
-                        <Button
-                            // onClick={onClose} 
-                            type='submit'
-                            variant="contained"
-                            color="primary"
-                            fullWidth
-                        >
-                            Войти
-                        </Button>
-                    </FormGroup>
-                </FormControl>
-            </form>
-        </ModalBlock>
-    );
+    const onSubmit = async (data: LoginModalFormProps) => {
+        try {
+            const userData = await AuthApi.signIn(data);
+            openNotificationRef.current('Авторизация успешна!', 'success');
+        } catch (error) {
+            openNotificationRef.current('Неверный логин или пароль', 'error');
+        };
+    };
+
+
+    return <Notification>
+        {
+            callback => {
+                openNotificationRef.current = callback;
+                return (
+                    <ModalBlock
+                        visible={open}
+                        onClose={(onClose)}
+                        classes={classes}
+                        title="Войти в аккаунт">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <FormControl className={classes.loginFormControl} component="fieldset" fullWidth>
+                                <FormGroup aria-label="position" row>
+                                    <Controller
+                                        control={control}
+                                        render={({ field: { onChange, onBlur, value } }) => (
+                                            <TextField
+                                                className={classes.loginSideField}
+                                                id="email"
+                                                label="E-Mail"
+                                                InputLabelProps={{
+                                                    shrink: true,
+                                                }}
+                                                variant="filled"
+                                                type="email"
+                                                onBlur={onBlur}
+                                                onChange={value => onChange(value)}
+                                                value={value}
+                                                helperText={errors.email?.message}
+                                                error={!!errors.email}
+                                                fullWidth
+                                                autoFocus
+                                            />
+                                        )}
+                                        name="email"
+                                        rules={{ required: true }}
+                                        defaultValue=""
+                                    />
+                                    <Controller
+                                        control={control}
+                                        render={({ field: { onChange, onBlur, value } }) => (
+                                            <TextField
+                                                className={classes.loginSideField}
+                                                id="password"
+                                                label="Пароль"
+                                                InputLabelProps={{
+                                                    shrink: true,
+                                                }}
+                                                variant="filled"
+                                                type="password"
+                                                onBlur={onBlur}
+                                                onChange={value => onChange(value)}
+                                                value={value}
+                                                helperText={errors.password?.message}
+                                                error={!!errors.password}
+                                                fullWidth
+                                            />
+                                        )}
+                                        name="password"
+                                        rules={{ required: true }}
+                                        defaultValue=""
+                                    />
+                                    <Button
+                                        // onClick={onClose} 
+                                        type='submit'
+                                        variant="contained"
+                                        color="primary"
+                                        fullWidth
+                                    >
+                                        Войти
+                            </Button>
+                                </FormGroup>
+                            </FormControl>
+                        </form>
+                    </ModalBlock>
+                )
+            }
+        }
+    </Notification>
 };
 
 export default LoginModal;
---
+++ frontend/src/services/api/authApi.ts
@@ -0,0 +1,24 @@
+import { LoginModalFormProps } from './../../pages/SignIn/components/LoginModal';
+import { axios } from '../../core/axios';
+// import { TagsState } from './../../store/ducks/tags/contracts/state';
+
+
+interface ResponseApi {
+    status: string;
+    data: any;
+};
+
+
+
+export const AuthApi = {
+    async signIn(postData: LoginModalFormProps): Promise<ResponseApi> {
+      const { data } = await axios.post<ResponseApi>('/auth/login', {username: postData.email, password: postData.password});
+        return data.data;
+    },
+
+    // async signIn(): Promise<void> {
+    //     const { data } = await axios.get<ResponseApi>('/auth/login');
+    //       return data.data;
+    //   },
+  };
+  
