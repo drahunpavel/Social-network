9c6d529b264a6a7be6821e1bd71f0bd5bc080886
drahunpavel <drahunpavel@gmail.com>
2021-06-12 01:14:43 +0300

    изменение роута, если пользователь имеет токен

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { SignIn } from './pages/SignIn/SignIn';
+import { AuthApi } from './services/api/authApi';
+import { setUserData } from './store/ducks/user/actionCreators';
+import { selectIsAuth } from './store/ducks/user/selectors';
 
 function App() {
+  // TODO: Поправить через саги или как-нибудь по другому, это херня
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  // TODO: Чекать если юзер не авторизован, то очищать токен и редакс
+  const checkAuth = async () => {
+    try {
+      const { data } = await AuthApi.getMe();
+      dispatch(setUserData(data));
+      // history.replace('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  React.useEffect(() => {
+    checkAuth();
+  }, []);
+
+  React.useEffect(() => {
+    if (isAuth) {
+      history.push('/home');
+    }
+  }, [isAuth]);
+
+
+
   return (
     <div className="App">
       <Switch>
